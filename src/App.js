8d6759e49639c455c491ad657de4a7c26a864f03
Muhammad Hassan <hassanchaudhry39@gmail.com>
2023-11-08 20:27:48 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import HomePage from "./HomePage.jsx/HomePage";
 import ExplorOffer from "./components/Offers/ExplorOffer";
 import IndustrySection from "./components/IndustrySection/IndustrySection";
 import MainImagesComponent from "./components/technologies/MainImagesComponent"
+import Slider  from "./components/Slider/Slider";
 const App = () => {
   return (
     <>
       <HomePage />
+      <Slider />
       <ExplorOffer />
       <IndustrySection />
       <MainImagesComponent />
